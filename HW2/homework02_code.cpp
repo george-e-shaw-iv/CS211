641c3e2bd7e006fbd7aef0a44d81eaeb51f4d762
George Shaw <george.e.shaw.iv@outlook.com>
2017-09-08 15:40:22 -0500

    hw2 triangle

--- HW2/homework02_code.cpp
+++ HW2/homework02_code.cpp
@@ -1,9 +1,9 @@
 /*
- * Filename: 
- * Name: 
- * WSUID: 
- * HW#: 
- * Description: 
+ * Filename: homework02_code.cpp
+ * Name: George Shaw
+ * WSUID: g829g576
+ * HW#: 2
+ * Description: Shapes using minimal cout statements.
  */
 
 #include <iostream>
@@ -11,5 +11,16 @@
 using namespace std;
 
 int main() {
+	//Triangle
+	for(int line = 0; line < 4; line++) {
+		for(int spaces = line; spaces > 0; spaces--) {
+			cout << " ";
+		}
+		for(int hashes = (8 - (line * 2)); hashes > 0; hashes--) {
+			cout << "#";
+		}
+		cout << "\n";
+	}
+
 	return 0;
 }
