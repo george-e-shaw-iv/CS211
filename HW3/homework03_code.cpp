0366f098665a2af2f095a21637e0165a4db0624f
George Shaw <george.e.shaw.iv@outlook.com>
2017-09-19 18:41:29 -0500

    init hw3 commit

---
+++ HW3/homework03_code.cpp
@@ -0,0 +1,26 @@
+/*
+ * Filename: homework03_code.cpp
+ * Name: George Shaw
+ * WSUID: g829g576
+ * HW#: 3
+ * Description: A loan calculator.
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	double loanAmount, interestRate, monthlyPayment;
+
+	cout << "Enter amount of loan: ";
+	cin >> loanAmount;
+
+	cout << "Enter interest rate: ";
+	cin >> interestRate;
+
+	cout << "Enter monthly payment: ";
+	cin >> monthlyPayment;
+
+	return 0;
+}
