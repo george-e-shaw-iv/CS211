d5331c74c947710d93ecf8184c03940b2ce20ef9
George Shaw <george.e.shaw.iv@outlook.com>
2017-10-05 17:55:56 -0500

    added comments to hw5

--- HW5/program5.cpp
+++ HW5/program5.cpp
@@ -16,11 +16,13 @@ const int SIZE = 5;
 const char dog[SIZE] = {'B', 'I', 'N', 'G', 'O'};
 
 int main() {
+	//Loop for main 6 verse blocks
 	for(int i = 0; i <= SIZE; i++) {
 		cout << "There was a farmer had a dog\nand ";
 		printArray(dog);
 		cout << "was his name-o\n";
 
+		//Loop for the 3 occurences of B-I-N-G-O per verse
 		for(int count = 0; count < 3; count++) {
 			for(int star = 0; star < i; star++) {
 				cout << "* ";
@@ -39,6 +41,7 @@ int main() {
 	return 0;
 }
 
+//Function to print array
 void printArray(const char arr[]) {
 	for(unsigned int i = 0; i <= (sizeof(arr)/sizeof(*arr)); i++) {
 		cout << arr[i] << " ";
