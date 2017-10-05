7fedd483bba34a2e0112cdfb26d321a83b8d7ae3
George Shaw <george.e.shaw.iv@outlook.com>
2017-10-05 17:53:29 -0500

    updated readme and hw5 init commit

---
+++ HW5/program5.cpp
@@ -0,0 +1,46 @@
+/*
+ * Filename: program5.cpp 
+ * Name: George Shaw
+ * WSUID: G829G576
+ * HW#: 5
+ * Description: A program that prints the lyrics to the song B-I-N-G-O.
+ */
+
+#include <iostream>
+
+using namespace std;
+
+void printArray(const char arr[]);
+
+const int SIZE = 5;
+const char dog[SIZE] = {'B', 'I', 'N', 'G', 'O'};
+
+int main() {
+	for(int i = 0; i <= SIZE; i++) {
+		cout << "There was a farmer had a dog\nand ";
+		printArray(dog);
+		cout << "was his name-o\n";
+
+		for(int count = 0; count < 3; count++) {
+			for(int star = 0; star < i; star++) {
+				cout << "* ";
+			}
+			for(int letter = i; letter < SIZE; letter++) {
+				cout << dog[letter] << " ";
+			}
+			cout << "\n";
+		}
+
+		cout << "And ";
+		printArray(dog);
+		cout << "was his name-o\n\n";
+	}
+
+	return 0;
+}
+
+void printArray(const char arr[]) {
+	for(unsigned int i = 0; i <= (sizeof(arr)/sizeof(*arr)); i++) {
+		cout << arr[i] << " ";
+	}
+}
