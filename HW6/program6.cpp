f00d09db71c3556fe3dc47d2ca18092e828f1422
George Shaw <george.e.shaw.iv@outlook.com>
2017-10-22 22:46:47 -0500

    fixed usage of cstring library

--- HW6/program6.cpp
+++ HW6/program6.cpp
@@ -7,8 +7,6 @@
  */
 
 #include <iostream>
-#include <ctype.h>
-#include <cstring>
 
 using namespace std;
 
@@ -20,10 +18,18 @@ void caesarDecode(char (&message)[SIZE], char delimeter, int cShift, int arrSize
 void printArray(char array[], char delimeter, int arrSize);
 
 int main() {
-	char message[SIZE];
+	char message[SIZE], ch;
+	int counter = 0;
 
 	cout << "Please enter a message (" << SIZE << " character limit): ";
-	cin.get(message, SIZE);
+	ch = cin.get();
+	while(ch != '\n' && counter <= 200) {
+		if(ch >= 'a' && ch <= 'z') ch -= 32;
+		message[counter] = ch;
+		counter++;
+		ch = cin.get();
+	}
+	message[counter] = '\0';
 	cout << endl;
 
 	// Uppercase the character array
